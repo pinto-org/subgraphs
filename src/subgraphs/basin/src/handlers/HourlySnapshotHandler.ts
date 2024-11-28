94de30dd3ca015b64e422234870c98b1fdbdff04
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2024-11-27 19:12:31 -0500

    Pinto seasons hourly snapshot

--- src/subgraphs/basin/src/handlers/HourlySnapshotHandler.ts
+++ src/subgraphs/basin/src/handlers/HourlySnapshotHandler.ts
@@ -3,9 +3,19 @@ import { checkForSnapshot } from "../utils/Well";
 import { toAddress } from "../../../../core/utils/Bytes";
 import { loadOrCreateAquifer } from "../entities/WellComponents";
 import { v } from "../utils/constants/Version";
-import { getAquifer } from "../../../../core/constants/RuntimeConstants";
+import { getAquifer, getWhitelistedWells } from "../../../../core/constants/RuntimeConstants";
+import { Sunrise } from "../../generated/Basin-ABIs/PintoLaunch";
 
-// Used to take hourly snapshots in the absense of pool trading activity.
+// Takes snapshots of beanstalk wells only. This is to guarantee a snapshot of each Beanstalk well is
+// always available at the top of the season.
+export function handleBeanstalkSunrise(event: Sunrise): void {
+  const wells = getWhitelistedWells(v());
+  for (let i = 0; i < wells.length; i++) {
+    checkForSnapshot(toAddress(wells[i]), event.block);
+  }
+}
+
+// Used to take hourly snapshots for other wells in the absense of pool trading activity.
 // This handler should be configured for infrequent polling
 export function handleBlock(block: ethereum.Block): void {
   const aquifer = loadOrCreateAquifer(getAquifer(v()));
