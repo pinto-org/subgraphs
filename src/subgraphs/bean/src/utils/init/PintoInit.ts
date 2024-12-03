adfcbc3001b61fdd426bf5d85012c7f255e54da7
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2024-12-03 00:20:28 -0500

    Fix season init

--- src/subgraphs/bean/src/utils/init/PintoInit.ts
+++ src/subgraphs/bean/src/utils/init/PintoInit.ts
@@ -6,7 +6,8 @@ import { createNewSeason } from "../../entities/Season";
 
 // Initialize season entity immediately/before first sunrise occurs.
 export function init(block: ethereum.Block): void {
+  const season = createNewSeason(1, block);
   const bean = loadBean(getProtocolToken(v(), block.number));
-  bean.lastSeason = createNewSeason(1, block).id;
+  bean.lastSeason = season.id;
   bean.save();
 }
