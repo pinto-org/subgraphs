77fb2089972b645fee84ac5b476e320b7aa94377
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2024-12-19 21:20:53 -0500

    init beanstalk entity

---
+++ src/subgraphs/basin/src/entities/Beanstalk.ts
@@ -0,0 +1,38 @@
+import { ethereum } from "@graphprotocol/graph-ts";
+import { Beanstalk, Season } from "../../generated/schema";
+import { ZERO_BD, ZERO_BI } from "../../../../core/utils/Decimals";
+
+export function createNewSeason(seasonNumber: u32, block: ethereum.Block): Season {
+  let season = Season.load(seasonNumber.toString());
+  if (season == null) {
+    season = new Season(seasonNumber.toString());
+    season.season = seasonNumber;
+    season.timestamp = block.timestamp;
+    season.save();
+  }
+  return season as Season;
+}
+
+export function loadBeanstalk(): Beanstalk {
+  let beanstalk = Beanstalk.load("beanstalk");
+  if (beanstalk == null) {
+    beanstalk = new Beanstalk("beanstalk");
+    beanstalk.lastSeason = "1";
+    beanstalk.wells = [];
+    beanstalk.totalLiquidityUSD = ZERO_BD;
+    beanstalk.cumulativeTradeVolumeUSD = ZERO_BD;
+    beanstalk.cumulativeDepositCount = 0;
+    beanstalk.cumulativeWithdrawCount = 0;
+    beanstalk.cumulativeSwapCount = 0;
+    beanstalk.rollingDailyTradeVolumeUSD = ZERO_BD;
+    beanstalk.rollingDailyTransferVolumeUSD = ZERO_BD;
+    beanstalk.rollingWeeklyTradeVolumeUSD = ZERO_BD;
+    beanstalk.rollingWeeklyTransferVolumeUSD = ZERO_BD;
+    // TODO: these might not be necessary if using beanstalk convention snapshots
+    beanstalk.lastSnapshotDayID = 0;
+    beanstalk.lastSnapshotHourID = 0;
+    beanstalk.lastUpdateTimestamp = ZERO_BI;
+    beanstalk.lastUpdateBlockNumber = ZERO_BI;
+  }
+  return beanstalk as Beanstalk;
+}
---
+++ src/subgraphs/basin/src/utils/init/PintoInit.ts
@@ -0,0 +1,11 @@
+import { ethereum } from "@graphprotocol/graph-ts";
+import { createNewSeason, loadBeanstalk } from "../../entities/Beanstalk";
+
+// Initialize season entity immediately/before first sunrise occurs.
+export function init(block: ethereum.Block): void {
+  createNewSeason(1, block);
+  const beanstalk = loadBeanstalk();
+  beanstalk.lastUpdateTimestamp = block.timestamp;
+  beanstalk.lastUpdateBlockNumber = block.number;
+  beanstalk.save();
+}
