fc28ad13e0d889f6ad97cfca0b5dc8bb21167c1b
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-02-27 02:19:43 -0500

    Fix manifest

--- src/subgraphs/beanstalk/src/handlers/WrappedSiloHandler.ts
+++ src/subgraphs/beanstalk/src/handlers/WrappedSiloHandler.ts
@@ -1,9 +1,7 @@
 import { ADDRESS_ZERO } from "../../../../core/utils/Bytes";
-import { ZERO_BI } from "../../../../core/utils/Decimals";
 import { Transfer } from "../../generated/Beanstalk-ABIs/WrappedSiloERC20";
 import { loadWrappedDeposit } from "../entities/Silo";
 import { takeWrappedDepositSnapshots } from "../entities/snapshots/WrappedSiloERC20";
-import { v } from "../utils/constants/Version";
 
 export function handleWrappedDepositERC20Transfer(event: Transfer): void {
   // Track overall supply
