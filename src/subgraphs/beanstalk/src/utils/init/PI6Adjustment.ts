04862c92d1de4f94fe98bb22a17704a2c388c43e
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-03-12 23:23:56 -0400

    Fix method signature

--- src/subgraphs/beanstalk/src/utils/init/PI6Adjustment.ts
+++ src/subgraphs/beanstalk/src/utils/init/PI6Adjustment.ts
@@ -5,7 +5,7 @@ import { takeSiloSnapshots } from "../../entities/snapshots/Silo";
 
 // Upon deployment of PI-6, the crop scalar was adjusted downward to 68e18 (From 100e18) but there was
 // no event emitted. This function handles what should have happened from that event.
-export function processMissingEvents(block: ethereum.Block) {
+export function processMissingEvents(block: ethereum.Block): void {
   let silo = loadSilo(BEANSTALK);
   silo.beanToMaxLpGpPerBdvRatio = BigInt.fromString("68000000000000000000");
   takeSiloSnapshots(silo, block);
