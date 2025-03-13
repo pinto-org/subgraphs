10e058d4037f30cb35c5628ad9db31eaeb9bb208
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-03-13 02:32:48 -0400

    Fix initial ratio

--- src/subgraphs/beanstalk/src/utils/init/PI6Adjustment.ts
+++ src/subgraphs/beanstalk/src/utils/init/PI6Adjustment.ts
@@ -3,11 +3,11 @@ import { loadSilo } from "../../entities/Silo";
 import { BEANSTALK } from "../../../../../core/constants/raw/PintoBaseConstants";
 import { takeSiloSnapshots } from "../../entities/snapshots/Silo";
 
-// Upon deployment of PI-6, the crop scalar was adjusted downward to 68e18 (From 100e18) but there was
+// Upon deployment of PI-6, the crop scalar was adjusted downward to 67e18 (From 100e18) but there was
 // no event emitted. This function handles what should have happened from that event.
 export function processMissingEvents(block: ethereum.Block): void {
   let silo = loadSilo(BEANSTALK);
-  silo.beanToMaxLpGpPerBdvRatio = BigInt.fromString("68000000000000000000");
+  silo.beanToMaxLpGpPerBdvRatio = BigInt.fromString("67000000000000000000");
   takeSiloSnapshots(silo, block);
   silo.save();
 }
